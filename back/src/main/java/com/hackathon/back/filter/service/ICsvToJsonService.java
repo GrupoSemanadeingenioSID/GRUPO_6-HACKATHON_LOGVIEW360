b90e23b915133fec8d3ac33f6e1aa833677e865d
Brayan Alejandro Riveros Rodriguez <brayan.riveros@igac.gov.co>
2025-05-30 14:26:47 -0500

    feat: logs

--- back/src/main/java/com/hackathon/back/filter/service/ICsvToJsonService.java
+++ back/src/main/java/com/hackathon/back/filter/service/ICsvToJsonService.java
@@ -1,10 +1,16 @@
 package com.hackathon.back.filter.service;
 
+import java.io.IOException;
 import java.util.List;
 
 import com.hackathon.back.dto.LogMidFlowESB;
 
-public interface ICsvToJsonService {
-    List<LogMidFlowESB> convertCsvToJson(String csvFilePath, String delimiter) throws Exception;
+<<<<<<< Updated upstream:back/src/main/java/com/hackathon/back/filter/service/ICsvToJsonService.java
+=======
+import java.io.IOException;
+import java.util.List;
 
+>>>>>>> Stashed changes:back/src/main/java/com/hackathon/back/service/ICsvToJsonService.java
+public interface ICsvToJsonService {
+    List<LogMidFlowESB> convertCsvToJson(String csvFilePath, String delimiter) throws IOException;
 }
--- back/src/main/java/com/hackathon/back/service/impl/CsvToJsonServiceImpl.java
+++ back/src/main/java/com/hackathon/back/service/impl/CsvToJsonServiceImpl.java
@@ -1,6 +1,3 @@
-<<<<<<< Updated upstream:back/src/main/java/com/hackathon/back/filter/service/impl/CsvToJsonServiceImpl.java
-package com.hackathon.back.filter.service.impl;
-=======
 package com.hackathon.back.service.impl;
 
 import com.hackathon.back.dto.LogMidFlowESB;
@@ -9,22 +6,12 @@ import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
->>>>>>> Stashed changes:back/src/main/java/com/hackathon/back/service/impl/CsvToJsonServiceImpl.java
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.List;
-
-import org.springframework.stereotype.Service;
-
-import com.hackathon.back.dto.LogMidFlowESB;
-import com.hackathon.back.filter.service.ICsvToJsonService;
-import com.opencsv.CSVReader;
-import com.opencsv.CSVReaderBuilder;
-
-import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
