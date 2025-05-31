cea0b9e6ef0b1be56a4f3c2dc5e5e106d79c77d1
Brayan Alejandro Riveros Rodriguez <brayan.riveros@igac.gov.co>
2025-05-30 19:14:51 -0500

    feat: aaaa

--- back/src/main/java/com/hackathon/back/config/SecurityConfig.java
+++ back/src/main/java/com/hackathon/back/config/SecurityConfig.java
@@ -55,10 +55,8 @@ public class SecurityConfig  {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("/api/v1/auth/**").permitAll()
-                        .requestMatchers("/api/v1/health").permitAll()
-                            .requestMatchers("/auth/**", "/v3/api-docs/**", "/swagger-ui/**","/swagger-ui.html").permitAll()
-                        .anyRequest().authenticated();
+                    auth.requestMatchers("/**").permitAll()
+                        .anyRequest().permitAll();
                 })
                 .cors(cors -> cors.configurationSource(configurationSource()))
                 .sessionManagement(session -> {
